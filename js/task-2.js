81ec7fc2d6ba3765bfb61399384afaa70470b3b1
kasjanchykn <kasjanchyk.n@gmail.com>
2024-02-23 15:54:14 +0200

    fix_task-3

--- js/task-2.js
+++ js/task-2.js
@@ -4,3 +4,4 @@ return (`Shipping to ${country} will cost ${price + deliveryFee} credit`)
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
